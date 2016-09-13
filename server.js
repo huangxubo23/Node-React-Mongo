9b4d4b63a8710a1acc9eaaa8a2bbbbda38e3a85b
huangxubo23 <732150595@qq.com>
2016-09-13 18:47:51 +0800

    Add socket.io

--- server.js
+++ server.js
@@ -16,14 +16,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res){
-  Router.run(routes, req.path, function(Handler){
+app.use(function (req, res) {
+  Router.run(routes, req.path, function (Handler) {
     var html = React.renderToString(React.createElement(Handler));
-    var page = swig.renderFile('views/index.html', { html: html});
+    var page = swig.renderFile('views/index.html', { html: html });
     res.send(page);
   });
 });
 
-app.listen(app.get('port'), function() {
+// app.listen(app.get('port'), function() {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function (socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function () {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  })
+});
+
+server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
